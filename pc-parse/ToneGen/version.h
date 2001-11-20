09d50492e8836f53274952d2cf63d804ad7d2741
Andy Black <andy_black@sil.org>
2001-11-20 16:14:10 +0100

    Version 1.0.4

--- pc-parse/ToneGen/version.h
+++ pc-parse/ToneGen/version.h
@@ -4,12 +4,14 @@
  */
 #define VERSION    1	  	/* major rewrite or expansion */
 #define REVISION   0		/* enhancement requiring adding to book */
-#define PATCHLEVEL -3		/* bug fix or other internal improvement */
-#define DATE       "December 27, 2000"	/* date of release version */
-#define YEAR       "2000"	/* used for release date and copyright */
+#define PATCHLEVEL -4		/* bug fix or other internal improvement */
+#define DATE       "November 20, 2001"	/* date of release version */
+#define YEAR       "2001"	/* used for release date and copyright */
 
 /*****************************************************************************
  * EDIT HISTORY
+ * 20-Nov-2001  hab - Recompiled with new changes to STAMP
+ * [1.0.4]
  * 27-Dec-2000  hab - Created DJGPP version
  * [1.0.3]
  * 19-Dec-2000  hab - fix bug in delete plural processing
