9ccdb46b56c40c64d4e9da7e8240a96bfc858f32
Andy Black <andy_black@sil.org>
2002-06-20 23:36:06 +0100

    Fix typo

--- pc-parse/pcpatr/StdAfx.cpp
+++ pc-parse/pcpatr/StdAfx.cpp
@@ -6,11 +6,11 @@
 
 #ifdef _ATL_STATIC_REGISTRY
 #include <statreg.h>
-#if _MSC_VER < 13
+#if _MSC_VER < 1300
 #include <statreg.cpp>
 #endif
 #endif
 
-#if _MSC_VER < 13
+#if _MSC_VER < 1300
 #include <atlimpl.cpp>
 #endif
