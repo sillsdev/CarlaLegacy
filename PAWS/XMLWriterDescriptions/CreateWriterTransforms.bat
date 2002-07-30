7c4ce296a13ec120fc84524f3e002fa13c8f2e29
Andy Black <andy_black@sil.org>
2002-07-31 00:48:52 +0100

    initial submission

---
+++ PAWS/XMLWriterDescriptions/CreateWriterTransforms.bat
@@ -0,0 +1,5 @@
+call CreateWriterTransform IntroAndTypology intro
+call CreateWriterTransform QP qp
+call CreateWriterTransform AdvP advp
+call CreateWriterTransform AdjP adjp
+call CreateWriterTransform NP np
