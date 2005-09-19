6c2875f4a6efb9144a134bbca6418a993f8ac3b4
Andy Black <andy_black@sil.org>
2005-09-19 22:17:07 +0100

    Initial submission

---
+++ PcPatrBrowser/PcPatrBrowserDll/DlgAbout.cs
@@ -0,0 +1,137 @@
+using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace SIL.PcPatrBrowser
+{
+	/// <summary>
+	/// Summary description for DlgAbout.
+	/// </summary>
+	public class DlgAbout : System.Windows.Forms.Form
+	{
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label lblVersion;
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.Container components = null;
+
+		public DlgAbout()
+		{
+			//
+			// Required for Windows Form Designer support
+			//
+			InitializeComponent();
+
+			lblVersion.Text = "Alpha Version " + ProductVersion;
+		}
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		protected override void Dispose( bool disposing )
+		{
+			if( disposing )
+			{
+				if(components != null)
+				{
+					components.Dispose();
+				}
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.Resources.ResourceManager resources = new System.Resources.ResourceManager(typeof(DlgAbout));
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.lblVersion = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			//
+			// button1
+			//
+			this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this.button1.Location = new System.Drawing.Point(120, 224);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(104, 24);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "&OK";
+			//
+			// label1
+			//
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
+			this.label1.Location = new System.Drawing.Point(24, 16);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(248, 24);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "PcPatr Browser";
+			//
+			// label2
+			//
+			this.label2.Location = new System.Drawing.Point(24, 40);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(296, 24);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "A tool to aid in studying syntactic tree output from PcPatr.";
+			//
+			// label3
+			//
+			this.label3.Location = new System.Drawing.Point(24, 104);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(188, 24);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "Copyright © 2005, SIL International";
+			//
+			// label4
+			//
+			this.label4.Location = new System.Drawing.Point(24, 160);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(296, 24);
+			this.label4.TabIndex = 4;
+			this.label4.Text = "For support, contact andy_black@sil.org";
+			//
+			// lblVersion
+			//
+			this.lblVersion.Location = new System.Drawing.Point(24, 64);
+			this.lblVersion.Name = "lblVersion";
+			this.lblVersion.Size = new System.Drawing.Size(256, 24);
+			this.lblVersion.TabIndex = 5;
+			this.lblVersion.Text = "Alpha Version";
+			//
+			// DlgAbout
+			//
+			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+			this.ClientSize = new System.Drawing.Size(344, 266);
+			this.Controls.Add(this.lblVersion);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "DlgAbout";
+			this.Text = "About PcPatrBrowser";
+			this.ResumeLayout(false);
+
+		}
+		#endregion
+	}
+}
---
+++ PcPatrBrowser/PcPatrBrowserDll/PcPatrDocument.cs
@@ -0,0 +1,215 @@
+using System;
+using System.IO;
+using System.Xml;
+
+namespace SIL.PcPatrBrowser
+{
+	/// <summary>
+	/// Summary description for PcPatrDocument.
+	/// </summary>
+	public class PcPatrDocument
+	{
+		protected Array m_aSentences;
+		protected string m_sAnaFile;
+		protected string m_sGrammarFile;
+		protected int m_iCurrentSentence;
+		public PcPatrDocument()
+		{
+		}
+		/// <summary>
+		/// constructor
+		/// </summary>
+		/// <param name="sAnaFile">AMPLE 'ana' file</param>
+		public PcPatrDocument(string sAnaFile, out string sGrammarFile)
+		{
+			m_sAnaFile = sAnaFile;
+			string sText = ReadFileIntoString(sAnaFile);
+
+			sGrammarFile = m_sGrammarFile = GetGrammarFile(sText);
+
+			int iCount = CountSentences(sText);
+			m_aSentences = Array.CreateInstance(typeof(PcPatrSentence), iCount);
+
+			ParseTextIntoSentences(sText);
+			m_iCurrentSentence = 0;
+		}
+
+		private string GetGrammarFile(string sText)
+		{
+			const string ksGrammarFileTemplate = @" Grammar file used: ";
+			int iTemplateLen = ksGrammarFileTemplate.Length;
+			int i = sText.IndexOf(ksGrammarFileTemplate);
+			if (i < 0)
+				return null;
+			int j = sText.Substring(i + iTemplateLen).IndexOf("\r\n");
+			string sFileName = sText.Substring(i + iTemplateLen, j).ToLower();;
+			if (!File.Exists(sFileName))
+				return null;
+			return sFileName;
+		}
+
+		private void ParseTextIntoSentences(string sText)
+		{
+			int iIndex = 0;
+			int iBegin = 0;
+			while (iBegin >= 0)
+			{
+				XmlNode sentenceNode = GetNextSentence(sText, ref iBegin);
+				if (sentenceNode != null)
+				{
+					PcPatrSentence sentence = new PcPatrSentence(sentenceNode);
+					m_aSentences.SetValue(sentence, iIndex++);
+				}
+			}
+		}
+
+		private int CountSentences(string sText)
+		{
+			int iCount = 0;
+			int iOffset = sText.IndexOf("<Analysis");
+			while (iOffset > 0)
+			{
+				iCount++;
+				iOffset = sText.IndexOf("<Analysis", iOffset + 9);
+			}
+			return iCount;
+		}
+		/// <summary>
+		/// Get currently selected sentence in the document
+		/// </summary>
+		public PcPatrSentence CurrentSentence
+		{
+			get
+			{
+				if (m_aSentences.Length > 0)
+					return (PcPatrSentence)m_aSentences.GetValue(m_iCurrentSentence);
+				else
+					return null;
+			}
+		}
+		/// <summary>
+		/// Get the number of the currently selected sentence in the document
+		/// </summary>
+		public int CurrentSentenceNumber
+		{
+			get
+			{
+				return m_iCurrentSentence+1;
+			}
+		}
+		/// <summary>
+		/// Get number of sentences in the document
+		/// </summary>
+		public int NumberOfSentences
+		{
+			get
+			{
+				if (m_aSentences != null)
+					return m_aSentences.Length;
+				else
+					return 0;
+			}
+		}
+		/// <summary>
+		/// Get the first sentence in the document
+		/// </summary>
+		public PcPatrSentence FirstSentence
+		{
+			get
+			{
+				m_iCurrentSentence = 0;
+				return CurrentSentence;
+			}
+		}
+		/// <summary>
+		/// Get the last sentence in the document
+		/// </summary>
+		public PcPatrSentence LastSentence
+		{
+			get
+			{
+				m_iCurrentSentence = m_aSentences.Length - 1;
+				return CurrentSentence;
+			}
+		}
+		/// <summary>
+		/// Get the next sentence in the document
+		/// </summary>
+		public PcPatrSentence NextSentence
+		{
+			get
+			{
+				m_iCurrentSentence = Math.Min(m_iCurrentSentence + 1, m_aSentences.Length - 1);
+				return CurrentSentence;
+			}
+		}
+		/// <summary>
+		/// Get the next sentence in the document
+		/// </summary>
+		public PcPatrSentence PreviousSentence
+		{
+			get
+			{
+				m_iCurrentSentence = Math.Max(m_iCurrentSentence - 1, 0);
+				return CurrentSentence;
+			}
+		}
+		/// <summary>
+		/// Get Sentences
+		/// </summary>
+		public Array Sentences
+		{
+			get
+			{
+				return m_aSentences;
+			}
+		}
+		/// <summary>
+		/// Get sentence based on its number
+		/// </summary>
+		/// <param name="iSentenceNumber">sentence number to get</param>
+		/// <returns></returns>
+		public PcPatrSentence GoToSentence(int iSentenceNumber)
+		{
+			if (iSentenceNumber <= 0)
+				return FirstSentence;
+			if (iSentenceNumber >= NumberOfSentences)
+				return LastSentence;
+			m_iCurrentSentence = iSentenceNumber - 1;
+			return CurrentSentence;
+		}
+		protected string ReadFileIntoString(string sFileName)
+		{
+			StreamReader sr = new StreamReader(sFileName);
+			return sr.ReadToEnd();
+		}
+		/// <summary>
+		/// Get XML of next analysis element in document
+		/// </summary>
+		/// <param name="sDoc">Document text file</param>
+		/// <param name="iBegin">index of where to begin looking;  is set at end of the next analysis; if negative, it indicates nothing was found</param>
+		/// <returns>xml node containing the analysis element</returns>
+		protected XmlNode GetNextSentence(string sDoc, ref int iBegin)
+		{
+			if (iBegin >= 0)
+			{
+				string sText = sDoc.Substring(iBegin);
+				int iBeg = sText.IndexOf("<Analysis");
+				int iEnd = sText.IndexOf("</Analysis>") + 13;  // 13 is for skipping past the </Analysis> + nl
+				if (iBeg > 0)
+				{
+					iBegin += iEnd;
+					string sXml = sText.Substring(iBeg, iEnd - iBeg);
+					XmlDocument doc = new XmlDocument();
+					doc.LoadXml(sXml);
+					return doc.SelectSingleNode("/");
+				}
+				else
+				{
+					iBegin = -1; // nothing to find; make sure index is negative
+				}
+			}
+			return null;
+		}
+	}
+}
